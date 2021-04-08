7b1731a8a47f39394a35e37e4299a5edc50ccf69
MongoosePainter <autongov@gmail.com>
2021-04-09 00:00:55 +0200

    Tore down Firebase Facebook Login for Facebook Gaming Services

--- MEVN/QuizApp/Client/src/api/FacebookService.js
+++ MEVN/QuizApp/Client/src/api/FacebookService.js
@@ -1,20 +1,34 @@
-import axios from 'axios';
 
-const url = 'https://graph.facebook.com/v10.0/';
 
 class FacebookService {
-    static getFriendsThatHaveApp(accessToken) {
-        axios.get(url + "me/" + "friends/" + "?access_token=" + accessToken)
-        .then(function (response) {
-        // handle success
-        console.log(response);
-        })
-        .catch(function (error) {
-        // handle error
-        console.log(error);
-        })
-        .then(function () {
-        // always executed
+    static init() {
+        window.fbAsyncInit = function() {
+          window.FB.init({
+            appId            : '3869199266499669',
+            autoLogAppEvents : true,
+            xfbml            : true,
+            version          : 'v2.10'
+          });
+          window.FB.AppEvents.logPageView();
+        };
+    
+        (function(d, s, id){
+          var js, fjs = d.getElementsByTagName(s)[0];
+          if (d.getElementById(id)) {return;}
+          js = d.createElement(s); js.id = id;
+          js.src = "//connect.facebook.net/en_US/sdk.js";
+          fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
+      }
+
+      static login() {
+        window.FB.login(function(response) {
+            if (response.authResponse) {
+                console.log("Nice");
+            } else {
+                console.log("Not nice");
+                // cancelled
+            }
         });
     }
 }
